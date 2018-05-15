10e038ab3e4bda8ad4946e8c910caac2f5002746
Daniel Lessa <38926235+DanielLessa96@users.noreply.github.com>
2018-05-15 14:36:37 -0300

    Update eh_primo.c

--- Exercicios Primos/eh_primo.c
+++ Exercicios Primos/eh_primo.c
@@ -3,18 +3,16 @@ int eh_primo (int n){
   int i;
   for (i=2;i<= n/2 ; i++){
   if ( n % i==0){
-	 return(0);}
-    }
+    return(0);}
+  }
   return(1);
 }
 
 int main() {
 
   int N;
-    printf("Entre com um numero para verificar se ele e primo:");
-    scanf ("%d", &N);
-
-
+  printf("Entre com um numero para verificar se ele e primo:");
+  scanf ("%d", &N);
   if (eh_primo(N)==0){
     printf ("Nao e primo\n");}
   else{
