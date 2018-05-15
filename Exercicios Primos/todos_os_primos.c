dae56810ee8c05e6ba9819c6c1530f9f994433b6
Daniel Lessa <38926235+DanielLessa96@users.noreply.github.com>
2018-05-15 14:37:46 -0300

    Update todos_os_primos.c

--- Exercicios Primos/todos_os_primos.c
+++ Exercicios Primos/todos_os_primos.c
@@ -2,9 +2,9 @@
 int todos_os_primos (int num)
 {
     int i;
-	for (i=2;i<= num/2 ; i++){
+    for (i=2;i<= num/2 ; i++){
         if ( num % i==0){
-	        return(0);}
+	    return(0);}
     }
     return(1);
 }
