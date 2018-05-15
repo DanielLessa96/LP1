9d756555455d368ea90dea04bdd6e93b071aeea2
Daniel Lessa <38926235+DanielLessa96@users.noreply.github.com>
2018-05-15 14:23:18 -0300

    Update todos_os_primos.c

--- Exercicios Primos/todos_os_primos.c
+++ Exercicios Primos/todos_os_primos.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
-
-
 int todos_os_primos (int num)
 {
-    int i, primo = 1;
-
-    for (i=2; i<num; i++)
-    {
-        if (num%i==0)
-            primo = 0;
+    int i;
+	for (i=2;i<= num/2 ; i++){
+        if ( num % i==0){
+	        return(0);}
     }
-
-    return primo;
+    return(1);
 }
 
 int main ()
 {
-    int max,i, cont = 1;
-    printf ("Entre com max para exibir todos os primos entre 1 e max:");
+    int max,j;
     scanf("%d", &max);
-
-    for (i=2; i<max; i++)
-    {
-        if (todos_os_primos(i))
+    for (j=2; j<max; j++){
+        if (todos_os_primos(j)==1)
         {
-            printf ("%d\n",i);
-            
-        }
+            printf ("%d ", j);}
     }
 }
-
