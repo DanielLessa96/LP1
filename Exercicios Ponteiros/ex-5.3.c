ccf2d3109d3c93a914c710d822aa7f83d1908cb9
Daniel Lessa <38926235+DanielLessa96@users.noreply.github.com>
2018-05-17 12:06:31 -0300

    Update ex-5.3.c

--- Exercicios Ponteiros/ex-5.3.c
+++ Exercicios Ponteiros/ex-5.3.c
@@ -4,22 +4,17 @@ void compra (int* conta, int valor){
 }
 
 int main(){
-	int minha_conta1,minha_conta2,qnt,i,valor;
+	int minha_conta1,minha_conta2,i,compras[]={100,50,80,30,20};
 	int* conta;
 	printf ("Entre com os saldos das contas:");
 	scanf ("%d %d", &minha_conta1,&minha_conta2);
-        printf ("Entre com a quantidade de compras:");
-	scanf ("%d", &qnt);
-	for (i=1;i<=qnt;i++){
-	    printf ("Entre com os valores das compras:");
-	    scanf ("%d", &valor);
-	    if(minha_conta1>=minha_conta2){
+	for (i=0;i<5;i++){
+	   if(minha_conta1>=minha_conta2){
 		    conta = &minha_conta1;
 	    }else{
 	        conta = &minha_conta2;
-        }
-            compra(conta, valor);
+            }
+            compra(conta, compras[i]);
 	    printf ("Saldo das contas apos a compra: %d %d\n",minha_conta1,minha_conta2);
-    }
-	
+        }
 }
