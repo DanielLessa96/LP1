5df16559328acb986fd9ce239dc973b0add49b35
Daniel Lessa <38926235+DanielLessa96@users.noreply.github.com>
2018-05-29 14:11:25 -0300

    Add files via upload

---
+++ Struct/struct2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+struct disciplina{
+    char disciplina[30];
+    float nota_prova1;
+    float nota_prova2; 
+};
+
+
+struct ficha_de_aluno{
+    char nome[50];
+    long int matricula;
+    struct disciplina dis;
+};
+
+
+void preenche(struct ficha_de_aluno* al) {
+    printf("Nome do aluno: ");
+    scanf ("%s", (*al).nome);
+    printf("Matricula: "); 
+    scanf ("%li", &(*al).matricula);
+    
+}
+ 
+void preenche2(struct ficha_de_aluno* al) {
+    preenche(al);
+    printf("Disciplina: "); 
+    scanf ("%s", (*al).dis.disciplina);
+    printf("Informe a 1a. nota: ");
+    scanf("%f", &(*al).dis.nota_prova1);
+    printf("Informe a 2a. nota: ");
+    scanf("%f", &(*al).dis.nota_prova2); 
+}
+int main(){
+    struct ficha_de_aluno al;
+    preenche2 (&al);
+    printf("\n\n --------- Dados do Aluno ---------\n\n");
+    printf("Nome: %s\n", al.nome);
+    printf("Matricula: %li\n" , al.matricula);
+    printf("Disciplina: %s\n", al.dis.disciplina);
+    printf("Nota da Prova 1: %.2f\n" , al.dis.nota_prova1);
+    printf("Nota da Prova 2: %.2f\n" , al.dis.nota_prova2);
+    return(0);
+} 
