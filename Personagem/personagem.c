54b113a2d1e04e6accef84429b50a2fcdfbd6588
Daniel Lessa <38926235+DanielLessa96@users.noreply.github.com>
2018-06-13 12:27:21 -0300

    Update personagem.c

--- Personagem/personagem.c
+++ Personagem/personagem.c
@@ -4,63 +4,66 @@
 #define C 10
 
 struct Posicao{
-      int x;
-      int y;
+    int x;
+    int y;
 };
 
 struct Personagem{
-      int identidade;
-      int pontos;
-      struct Posicao p;
+    int identidade;
+    int pontos;
+    struct Posicao p;
 };
 
 void ler_pos(struct Posicao* pos){
-        printf ("Posicao X:");
-        scanf ("%d", &(*pos).x);
-        printf ("Posicao Y:");
-        scanf ("%d", &(*pos).y);
+    printf ("Posicao X:");
+    scanf ("%d", &(*pos).x);
+    printf ("Posicao Y:");
+    scanf ("%d", &(*pos).y);
 }
 
 void ler_per(struct Personagem* per){ 
-        printf("\n\n --------- Dados do Personagem ---------\n\n");
-        printf ("Identidade:");
-        scanf ("%d", & (*per).identidade);
-        printf ("pontuacao:");
-        scanf ("%d", & (*per).pontos);
-        ler_pos(& (*per).p);
+    printf("\n\n --------- Dados do Personagem ---------\n\n");
+    printf ("Identidade:");
+    scanf ("%d", & (*per).identidade);
+    printf ("pontuacao:");
+    scanf ("%d", & (*per).pontos);
+    ler_pos(& (*per).p);
 }
-
 
 int main(){
-      int i,j,x,y,id;
-      int mat[L][C];
-      struct Personagem per[10];
-      for (i=0; i<L; i++){
-          ler_per (&per[i]);
-      }
-      printf("  ");
-      for (j=0; j<C; j++){
-          printf("%d ",j);
-      }
-      printf("\n");
-      for (i=0; i<L; i++){
-          printf("%d ",i);
-          x = per[i].p.x;
-          y = per[i].p.y;
-          id = per[i].identidade;
-          for (j=0; j<C; j++){
-              mat[i][j]=0;
-              if (i == x && j == y){
-                  mat[i][j] = id;
-              }
-              if (mat[i][j]== 0){
-                  printf("* ");
-              } else {
-                  printf("%d ", mat[i][j]);
-              }
-          }
-          printf("\n"); 
-      }
-      return(0); 
+    int i,j,x,y,id;
+    int mat[L][C];
+    struct Personagem per[10];
+    for (i=0; i<L; i++){
+        ler_per (&per[i]);
+        x = per[i].p.x;
+        y = per[i].p.y;
+        id = per[i].identidade;
+        for (j=0; j<C; j++){
+        	if (i == x && j == y){
+                mat[i][j] = id;
+            }else{
+            	mat[i][j]=0;
+		}
+        }
+    }
+    printf("  ");
+    for (j=0; j<C; j++){
+        printf("%d ",j);
+    }
+    printf("\n");
+    for (i=0; i<L; i++){
+        printf("%d ",i);
+        for (j=0; j<C; j++){
+            if (mat[i][j]== 0){
+                printf("* ");
+            } else {
+                printf("%d ", mat[i][j]);
+            }
+        }
+        printf("\n"); 
+    }
+    return(0); 
 }
+
 
